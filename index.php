db7fc368894bba6fce916b4cedddac2dce200147
Shariful Islam <shariful.stu20181@juniv.edu>
2022-03-24 11:27:20 +0600

    ..

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ if (isset($_POST['add_to_cart'])) {
    $product_name = $_POST['product_name'];
    $product_price = $_POST['product_price'];
    $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
+   // $product_quantity = $_POST['product_quantity'];
 
    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if (mysqli_num_rows($check_cart_numbers) > 0) {
       $message[] = 'Already added to cart!';
    } else {
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image) VALUES('$user_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
       $message[] = 'Product added to cart!';
    }
 }
